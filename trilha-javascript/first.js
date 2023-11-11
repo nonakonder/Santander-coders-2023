60d40fb36b53b54883b21d3d2e9829ba996faa4f
Joanna Konder <149900886+nonakonder@users.noreply.github.com>
2023-11-11 10:22:13 -0300

    add javascript file

--- trilha-javascript/first.js
+++ trilha-javascript/first.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!')
+//alert("Estou aprendendo JavaScript")
+//alert("mensagem") envia uma mensagem estilo pop-up para o navegador.
+console.log("Heello querida")
+//console.log("mensagem") console. = seta uma ação para o console do navegador, no caso o .log("") vai indicar uma mensagem para aparecer no console.
+
+const nome = prompt("Qual o seu nome?")
+var idade = prompt ("Qual a sua idade?")
+
+idade = parseInt(idade)
+
+alert(`Seu nome é ${nome} e ano que vem você terá ${idade + 1} anos`)
+
